1c47dfbc91c43f91ee95a3ac87b2409a75f5fa8a
neelb99 <neelbajaj1234@hotmail.com>
2019-09-06 20:20:28 +0530

    initial deploy

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
+app.use(express.static(path.join(__dirname,"client","build")))
 app.use(express.json());
 app.use(cors());
 
@@ -13,5 +15,9 @@ const uri =process.env.DB_URI;
 mongoose.connect(uri,{useCreateIndex:true, useNewUrlParser:true});
 const connection = mongoose.connection;
 connection.once('open',()=>console.log("DB connected"));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"));
+});
 
 app.listen(PORT,()=>console.log("Server running"));
